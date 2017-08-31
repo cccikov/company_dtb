3880a688372bbfe9f9463cc3f6ebcd16e70b44eb
ccc <522978139@qq.com>
2017-08-31 16:26:58 +0800

    init

---
+++ web/js/base.js
@@ -0,0 +1,11 @@
+function baseFontSize() {
+    var maxFontSize = 20;
+    var rootfontsize = window.innerWidth / 20;
+    rootfontsize = rootfontsize > maxFontSize ? maxFontSize : rootfontsize;
+    document.documentElement.style.fontSize = rootfontsize + "px"
+    return rootfontsize;
+}
+var rem = baseFontSize();
+window.addEventListener("resize", function() {
+    rem = baseFontSize();
+}, false);
