74058f19b5ac0224d4e5728e3c91aa2782a9549b
ccc <522978139@qq.com>
2017-12-07 18:05:36 +0800

    项目详情

--- m/js/project_detail.js
+++ m/js/project_detail.js
@@ -5,26 +5,36 @@ var vm = new Vue({
         projectListData: {},
     },
     methods: {
-        getdata: function() {
-            $.get("/m/getprojdetail?pjid=" + id, function(r) {
+        getdata: function () {
+            $.get("/m/data/projectDetailData.json", function (r) {
+                r = JSON.stringify(r);
+                
                 var result = JSON.parse(r);
                 vm.projectListData = result.pjInfoEntity;
                 vm.$mount("#wrap");
             });
         },
-        unescape:function(str) {
-        	 var arrEntities={'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"'};
-        	 return str.replace(/&(lt|gt|nbsp|amp|quot);/ig,function(all,t){return arrEntities[t];});
-        	}
+        unescape: function (str) {
+            var arrEntities = {
+                'lt': '<',
+                'gt': '>',
+                'nbsp': ' ',
+                'amp': '&',
+                'quot': '"'
+            };
+            return str.replace(/&(lt|gt|nbsp|amp|quot);/ig, function (all, t) {
+                return arrEntities[t];
+            });
+        }
     },
     computed: {
-        page: function() {
+        page: function () {
             return 2;
         }
     },
-    mounted: function() {
+    mounted: function () {
         var _this = this;
-        Vue.nextTick(function() { // Vue初始化完成
+        Vue.nextTick(function () { // Vue初始化完成
             alinkAnimation();
         });
     },
