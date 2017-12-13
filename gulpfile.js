4e2218171e45229a4172448d87f17d7da52ef798
ccc <522978139@qq.com>
2017-12-13 18:55:10 +0800

    什么都改

--- gulpfile.js
+++ gulpfile.js
@@ -347,7 +347,7 @@ gulp.task('clear', function (done) {
 // 不使用变量 和 函数 , 为了让以后更好理解
 
 /* 手机端 */
-gulp.task("m_less", function () { // gulp less 
+gulp.task("m_less", function () { // gulp less
 
     gulp.src("m/less/**/*.less", {
             base: "m/less"
@@ -392,5 +392,43 @@ gulp.task('m_syncless', function () {
             stream: true
         }));
     });
-    
+
+});
+
+
+gulp.task('web_syncless', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./web",
+            index: "index.html"
+        },
+        port: 3000,
+        ui: {// ui的默认端口
+            port: 3001,
+            weinre: { // 不知道什么鬼 "weinre"好像也是用于远程调试的nodejs工具
+                port: 3002
+            }
+        }
+    });
+
+    // 转换less
+    gulp.watch("web/less/**/*.less").on('change', function (event) {
+        gulp.src("web/less/**/*.less", { // 这个是全部css变化且刷新
+                base: "web/less"
+            })
+            .pipe(less())
+            .pipe(gulp.dest("web/css"))
+            .pipe(browserSync.reload({
+                stream: true
+            }));;
+    });
+
+    // 监视文件变化同步浏览器
+    gulp.watch(["web/**/*.html", "web/js/*.js"]).on("change", function (event) {
+        gulp.src(event.path).pipe(browserSync.reload({
+            stream: true
+        }));
+    });
+
 });
