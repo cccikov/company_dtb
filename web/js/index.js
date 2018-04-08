fb181e49edfa0b56ada032022bf6fbc57d2a5b46
ccc <522978139@qq.com>
2018-04-08 16:54:03 +0800

    首页

--- web/js/index.js
+++ web/js/index.js
@@ -1,16 +1,43 @@
-$(function(){
-    // 轮播
-    var mySwiper = new Swiper ('.swiper-container', {
-        effect : 'fade', // 切换效果
-        loop:true,
-        autoplay:2000,
-        // 如果需要分页器
-        pagination: '.swiper-pagination',
+$(function() {
+
+
+    new Vue({
+        "el": "#wrap",
+        mounted: function() {
+            this.$nextTick(function() {
+                onload();
+            });
+        }
     });
 
-    // tab切换
-    var tab = new Tab({
-        btn:$(".news-tab-btn a"),
-        box:$(".news-tab-box"),
-    });
+    function onload() {
+
+        // 轮播
+        var mySwiper = new Swiper('.swiper-container', {
+            // effect: 'fade', // 切换效果
+            // loop: true,
+            autoplay: 5000,
+            autoHeight: true,
+            // 如果需要分页器
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            onInit: function(swiper) {
+                swiper.slides.height(swiper.width * 0.2604166666666667);
+            }
+        });
+
+
+        $(".progress-canvas").circleProgress({
+            size: 76,
+            startAngle: -Math.PI / 2,
+            thickness: 5,
+            fill: {
+                gradient: ["#ff8a35", "#ff3b35"],
+            },
+        });
+
+    }
+
+
+
 });
