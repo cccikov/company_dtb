b523fba575fef0154c791018717dacd6509c7d9b
ccc <522978139@qq.com>
2018-06-14 18:05:40 +0800

    认证

--- m/js/cert.js
+++ m/js/cert.js
@@ -1,11 +1,11 @@
 var vm = new Vue({
     el: "#wrap",
     data: {
-
+        certified: true,
     },
     methods: {
-
+        certify: function () {
+            this.certified = false;
+        }
     }
 });
-
-
