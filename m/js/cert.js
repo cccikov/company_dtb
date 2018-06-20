1f49f21e337e266e66c0b7e0578ddaa6e8f53b72
ccc <522978139@qq.com>
2018-06-20 11:50:24 +0800

    投资人认证

--- m/js/cert.js
+++ m/js/cert.js
@@ -5,7 +5,8 @@ var vm = new Vue({
     el: "#wrap",
     data: {
         certified: false,
-        type: "add"
+        type: "select",
+        cardid: 0
     },
     methods: {
         certify: function () {
@@ -13,25 +14,42 @@ var vm = new Vue({
         },
         change_type: function (type) {
             this.type = type;
+            this.clearerror(); // 清除验证错误
         },
-        formatBankCard: function (e) {
+        bankCardInput: function (e) {
             var el = e.currentTarget;
-            console.log(e);
-            // formatBankCard();
+            el.value = this.formatBankCard(el.value);
+        },
+        getCode: function () {
+            if (this.type == "select") {
+                this.manual("name|phone|id|font|back");
+                var result = this.group("name|phone|id|font|back");
+            } else if (this.type == "add") {
+                this.manual("name|phone|id|font|back|cardno|bank|bankadd");
+                var result = this.group("name|phone|id|font|back|cardno|bank|bankadd");
+            }
+
+            if (result) {
+                alert("获取验证码");
+            } else {
+                return
+            }
+        },
+        selectCard: function (cardid) {
+            this.cardid = cardid;
+        },
+        formatBankCard: function (card, spacing) { // 格式化银行卡
+            card = String(card);
+            card = card.replace(/\s/g, "");
+            var len = card.length;
+            var spacing = spacing || 4;
+            var arr = [];
+            var n = 0;
+            while (n < len) {
+                arr.push(card.slice(n, n + spacing));
+                n += spacing;
+            };
+            return arr.join(" ");
         }
     }
 });
-
-function formatBankCard(card, spacing) {
-    card = String(card);
-    card = card.replace(/\s/g, "");
-    var len = card.length;
-    var spacing = spacing || 4;
-    var arr = [];
-    var n = 0;
-    while (n < len) {
-        arr.push(card.slice(n, n + spacing));
-        n += spacing;
-    };
-    return arr.join(" ");
-}
